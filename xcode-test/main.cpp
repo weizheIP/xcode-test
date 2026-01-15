78390288ba6012bfe38e7535a8fac3d7235c5c87
weizheIP <2106405330@qq.com>
2026-01-16 00:19:33 +0800

    Initial Commit

---
+++ xcode-test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  xcode-test
+//
+//  Created by Zhuanz1 on 2026/1/16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
